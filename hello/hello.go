4a41847675ecbd93e48c4cc7fa2df2c8cae32fdd
Leonardo Marcelino <leonardo.marcelino@gmail.com>
2020-11-18 19:11:08 -0300

    Feature to allow to say Hello in Spanish

--- hello/hello.go
+++ hello/hello.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
+const english = "English"
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World", english))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -13,15 +13,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Leo")
+		got := Hello("Leo", english)
 		want := "Hello, Leo"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", english)
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("saying hello to people in Spanish", func(t *testing.T) {
+		got := Hello("Leo", spanish)
+		want := "Hola, Leo"
+		assertCorrectMessage(t, got, want)
+	})
 }
