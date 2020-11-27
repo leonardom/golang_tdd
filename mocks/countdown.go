e9b3d1ab4b1fc680583baacc466667d4857da11b
Leonardo Marcelino <leonardo.marcelino@gmail.com>
2020-11-27 15:41:57 -0300

    Learning about Mocks

---
+++ mocks/countdown.go
@@ -0,0 +1,32 @@
+package mocks
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
---
+++ mocks/countdown_test.go
@@ -0,0 +1,95 @@
+package mocks
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+	"time"
+)
+
+const sleep = "sleep"
+const write = "write"
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (c *CountdownOperationsSpy) Sleep() {
+	c.Calls = append(c.Calls, sleep)
+}
+
+func (c *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	c.Calls = append(c.Calls, write)
+	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func TestCountdown(t *testing.T) {
+	t.Run("prints 3 to Go", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
+
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		expected := `3
+2
+1
+Go!`
+
+		if got != expected {
+			t.Errorf("Expected %q but got %q", expected, got)
+		}
+
+		if spySleeper.Calls != 4 {
+			t.Errorf("not enough calls to sleepr, want 4 got %d", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		expected := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(expected, spySleepPrinter.Calls) {
+			t.Errorf("Expected calls %v but got %v", expected, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
+}
